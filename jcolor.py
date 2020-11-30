e02dcda5c7a2d423aee64bc1fcd4b964fa8e9aaa
jay ess <mentoce@gmail.com>
2020-11-29 23:45:19 -0800

    Change class names, transform class variable names to lowercase, use color names from wikipedia

--- jcolor.py
+++ jcolor.py
@@ -7,67 +7,66 @@ import os
 os.system("")
 
 
-class Color:
-    BLACK = '\33[30m'
-    RED = '\33[31m'
-    GREEN = '\33[32m'
-    YELLOW = '\33[33m'
-    BLUE = '\33[34m'
-    VIOLET = '\33[35m'
-    BEIGE = '\33[36m'
-    WHITE2 = '\33[37m'
+class jcolor:
+    black = '\33[30m'
+    red = '\33[31m'
+    green = '\33[32m'
+    yellow = '\33[33m'
+    blue = '\33[34m'
+    magenta = '\33[35m'
+    cyan = '\33[36m'
+    white = '\33[37m'
+    gray = '\33[90m'
 
-    GREY = '\33[90m'
-    RED2 = '\33[91m'
-    GREEN2 = '\33[92m'
-    YELLOW2 = '\33[93m'
-    BLUE2 = '\33[94m'
-    VIOLET2 = '\33[95m'
-    BEIGE2 = '\33[96m'
-    WHITE = '\33[97m'
-
-    def __add__(cls, other):
-        return cls.value + other.value
-
-
-class Style:
-    RESET = '\33[0m'
-    BOLD = '\33[1m'
-    ITALICS = '\33[3m'
-    UNDERLINE = '\33[4m'
-    BLINK = '\33[5m'
-    BLINK2 = '\33[6m'
-    SELECTED = '\33[7m'
-    STRIKE = '\33[8m'
+    bright_red = '\33[91m'
+    bright_green = '\33[92m'
+    bright_yellow = '\33[93m'
+    bright_blue = '\33[94m'
+    bright_magenta = '\33[95m'
+    bright_beige = '\33[96m'
+    bright_white = '\33[97m'
 
     def __add__(cls, other):
         return cls.value + other.value
 
 
-class Background:
-    BLACK = '\33[40m'
-    RED = '\33[41m'
-    GREEN = '\33[42m'
-    YELLOW = '\33[43m'
-    BLUE = '\33[44m'
-    VIOLET = '\33[45m'
-    BEIGE = '\33[46m'
-    WHITE = '\33[47m'
-
-    GREY = '\33[100m'
-    RED2 = '\33[101m'
-    GREEN2 = '\33[102m'
-    YELLOW2 = '\33[103m'
-    BLUE2 = '\33[104m'
-    VIOLET2 = '\33[105m'
-    BEIGE2 = '\33[106m'
-    WHITE2 = '\33[107m'
+class jstyle:
+    reset = '\33[0m'
+    bold = '\33[1m'
+    italics = '\33[3m'
+    underline = '\33[4m'
+    blink = '\33[5m'
+    selected = '\33[7m'
+    strike = '\33[8m'
 
     def __add__(cls, other):
         return cls.value + other.value
 
 
-def colorize(msg, *args):
-    reset = Style.RESET
+class jbackground:
+    black = '\33[40m'
+    red = '\33[41m'
+    green = '\33[42m'
+    yellow = '\33[43m'
+    blue = '\33[44m'
+    magenta = '\33[45m'
+    cyan = '\33[46m'
+    white = '\33[47m'
+    gray = '\33[100m'
+
+    bright_red = '\33[101m'
+    bright_green = '\33[102m'
+    bright_yellow = '\33[103m'
+    bright_blue = '\33[104m'
+    bright_magenta = '\33[105m'
+    bright_cyan = '\33[106m'
+    bright_white = '\33[107m'
+
+    def __add__(cls, other):
+        return cls.value + other.value
+
+
+def jcolorize(msg, *args):
+    reset = jstyle.reset
     styles = ''.join(args)
     return styles + msg + reset
