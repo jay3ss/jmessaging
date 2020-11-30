4cc7a43edc6745b2caf612ca74be0a0ee8415c6f
jay ess <mentoce@gmail.com>
2020-11-30 14:19:56 -0800

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import pathlib
+from setuptools import find_packages, setup
+
+
+here = pathlib.Path(__file__).parent
+readme = (here/'README.md').read_text()
+
+setup(
+    name='jmessaging',
+    author='Jay Ess',
+    version='0.1.0',
+    description='Make printing colorized messages to terminal easier',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jay3ss/jmessaging',
+    packages=find_packages()
+)
