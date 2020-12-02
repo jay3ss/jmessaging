2168df31a4f7dca4c3079d0fbe0c4529cc02ae3e
jay ess <mentoce@gmail.com>
2020-12-01 20:51:56 -0800

    Fix incorrect importing of jprint module

--- jmessaging/__init__.py
+++ jmessaging/__init__.py
@@ -1,5 +1,6 @@
-from .jcolor import jbackground, jcolor, jcolorize, jprint, jstyle
+from .jcolor import jbackground, jcolor, jcolorize, jstyle
 from .jmessaging import Messenger
+from .jprint import print_same_line
 
 
 __version__ = '0.1.0'
