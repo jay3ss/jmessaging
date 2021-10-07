a001cf7e03d034f9dfefb527f5f9b3f6f211811d
jay ess <mentoce@gmail.com>
2021-10-06 18:38:41 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme = (here/'README.md').read_text()
 setup(
     name='jmessaging',
     author='Jay Ess',
-    version='0.1.0',
+    version='0.2.0',
     description='Make printing colorized messages to terminal easier',
     long_description=readme,
     long_description_content_type='text/markdown',
