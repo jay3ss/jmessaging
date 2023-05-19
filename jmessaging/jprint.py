b4121c65f003ccc561175d01ec804bde73c441e2
jay3ss <mentoce@gmail.com>
2023-05-19 13:48:40 -0700

    Update docstring

--- jmessaging/jprint.py
+++ jmessaging/jprint.py
@@ -7,7 +7,7 @@ def print_same_line(*msg):
     Args:
         msg: The message to be printed
 
-    Got some help from https://stackoverflow.com/a/5419488/3562890
+    Got some help from https://stackoverflow.com/a/5419488
     """
     clear = '\x1b[1K\r'
     print(clear, *msg, end='', flush=True, sep='')
