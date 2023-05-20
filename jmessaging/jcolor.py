a7dc3901466146f0b7439185d04d3a6a53849912
jay3ss <mentoce@gmail.com>
2023-05-19 20:17:29 -0700

    Add map from name to color/style

--- jmessaging/jcolor.py
+++ jmessaging/jcolor.py
@@ -30,6 +30,26 @@ class jcolor:
     bright_cyan = '\33[96m'
     bright_white = '\33[97m'
 
+    name_map = {
+        "\33[30m": "black",
+        "\33[31m": "red",
+        "\33[32m": "green",
+        "\33[33m": "yellow",
+        "\33[34m": "blue",
+        "\33[35m": "magenta",
+        "\33[36m": "cyan",
+        "\33[37m": "white",
+        "\33[39m": "default",
+        "\33[90m": "gray",
+        "\33[91m": "bright_red",
+        "\33[92m": "bright_green",
+        "\33[93m": "bright_yellow",
+        "\33[94m": "bright_blue",
+        "\33[95m": "bright_magenta",
+        "\33[96m": "bright_cyan",
+        "\33[97m": "bright_white",
+    }
+
     def __add__(cls, other):
         return cls.value + other.value
 
@@ -45,6 +65,16 @@ class jstyle:
     blink = '\33[5m'
     selected = '\33[7m'
     strike = '\33[8m'
+
+    name_map = {
+        "\33[0m": "reset",
+        "\33[1m": "bold",
+        "\33[3m": "italics",
+        "\33[4m": "underline",
+        "\33[5m": "blink",
+        "\33[7m": "selected",
+        "\33[8m": "strike",
+    }
 
     def __add__(cls, other):
         return cls.value + other.value
@@ -71,6 +101,25 @@ class jbackground:
     bright_magenta = '\33[105m'
     bright_cyan = '\33[106m'
     bright_white = '\33[107m'
+
+    name_map = {
+        "\33[40m": "black",
+        "\33[41m": "red",
+        "\33[42m": "green",
+        "\33[43m": "yellow",
+        "\33[44m": "blue",
+        "\33[45m": "magenta",
+        "\33[46m": "cyan",
+        "\33[47m": "white",
+        "\33[100m": "gray",
+        "\33[101m": "bright_red",
+        "\33[102m": "bright_green",
+        "\33[103m": "bright_yellow",
+        "\33[104m": "bright_blue",
+        "\33[105m": "bright_magenta",
+        "\33[106m": "bright_cyan",
+        "\33[107m": "bright_white",
+    }
 
     def __add__(cls, other):
         return cls.value + other.value
