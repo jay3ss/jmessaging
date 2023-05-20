125807f06f7fa57e43999ab5c0a9d4ed747f485a
jay3ss <mentoce@gmail.com>
2023-05-19 20:15:44 -0700

    jmessaging.py -> jmessage.py

--- jmessaging/jmessage.py
+++ jmessaging/jmessage.py
@@ -1,6 +1,97 @@
 """Simple, jcolorized messaging"""
+from typing import Any
+
+import jmessaging.exceptions as exc
 from jmessaging.jcolor import jcolor, jcolorize, jstyle
 from jmessaging.jprint import print_same_line
+
+
+class Message:
+    """
+    Class for messages
+    """
+
+    _left = "["
+    _right = "]"
+
+    def __init__(
+        self,
+        color: str = "default",
+        style: str = "bold",
+        background: str = "black"
+    ) -> None:
+        try:
+            self._color = getattr(jcolor, color)
+        except AttributeError as e:
+            raise exc.InvalidColor(e)
+
+        try:
+            self._style = getattr(jstyle, style)
+        except AttributeError as e:
+            raise exc.InvalidStyle(e)
+
+        try:
+            self._background = getattr(jcolor, background)
+        except AttributeError as e:
+            raise exc.InvalidBackground(e)
+
+    @property
+    def color(self) -> str:
+        return self._color
+
+    @color.setter
+    def color(self, new_color: str) -> None:
+        self._color = new_color
+
+    @property
+    def background(self) -> str:
+        return self._background
+
+    @background.setter
+    def background(self, new_background: str) -> None:
+        self._background = new_background
+
+    @property
+    def style(self) -> str:
+        return self._style
+
+    @style.setter
+    def style(self, new_style: str) -> None:
+        self._style = new_style
+
+    def __brackets(self, text: str):
+        """
+        Applies opening and closing brackets to the message level color (i.e.,
+        `info`, `warning`, `error`)
+
+        Args:
+            level (dict): dict containing the message level color (i.e., `info`,
+            `warning`, `error`) and the text (message) that gives the level of
+            the message
+
+        Returns:
+            str: the message level string with the surrounding brackets and text
+            color applied
+        """
+        left = jcolorize(self._left, self.color)
+        right = jcolorize(f"{self._right}:", self.color)
+        colorized_text = jcolorize(text)
+        return "".join([left, colorized_text, right])
+
+    def __call__(self, text: str) -> Any:
+        brackets = self.__brackets(text)
+        colorized_text = f"{brackets} {jcolorize(text, self.color)}"
+        return colorized_text
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        color = jcolor.name_map[self.color]
+        style = jstyle.name_map[self.style]
+        background = jcolor.name_map[self.background]
+        string = f"<Message color: {color}, style: {style}, bg: {background}>"
+        return string
 
 
 class Messenger:
@@ -96,3 +187,8 @@ class Messenger:
             print(text)
         else:
             print_same_line(text)
+
+
+if __name__ == "__main__":
+    msg = Message(color="invalid")
+    print()
