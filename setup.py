a349c2987c0b7b37e43ee50a3eb402629db298f9
jay3ss <mentoce@gmail.com>
2023-06-12 17:12:27 -0700

    Add extras_require attribute (read in from requirements-dev.txt), use version of package found in __init__.py file

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import pathlib
 from setuptools import find_packages, setup
 
+from jmessaging import __version__ as version
+
 
 here = pathlib.Path(__file__).parent
-readme = (here / "README.md").read_text()
+readme = (here / "README.md").read_text().splitlines()
+extras_require = {"dev": (here / "requirements-dev.txt").read_text()}
 
 setup(
     name="jmessaging",
     author="Jay Ess",
-    version="0.2.0",
+    version=version,
     description="Make printing colorized messages to terminal easier",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -16,4 +19,5 @@ setup(
     packages=find_packages(),
     license="MIT",
     include_package_data=True,
+    extras_require=extras_require,
 )
